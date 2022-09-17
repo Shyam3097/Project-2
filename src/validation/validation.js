b714a58fdaca6dfcda69636bfb498239c28b6ca5
Shyam3097 <97452238+Shyam3097@users.noreply.github.com>
2022-09-17 22:09:36 +0530

    modified validation

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -10,9 +10,6 @@ function Boolean (value) {
 
 const collegeValidation = async function (req, res, next) {
     try {
-        if (req.query) {
-            return res.status(400).send("Invalid request........!!!");
-        }
         let data = req.body;
         let { name, fullName, logoLink, isDeleted } = data
 
